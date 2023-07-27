bbe489bfc02df4365457828675cda215f174f513
Ankush Jain <64203644+ankushh21@users.noreply.github.com>
2023-07-27 23:33:55 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ExelaBase64Converter',
+    version='0.1.0',
+    author='Exela Technologies',
+    description='This package converts text pdf to base64 json.',
+    # packages=find_packages(),
+    install_requires=[
+        'ExelaBase64Converter',
+    ],
+)
